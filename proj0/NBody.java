5f7cef5e27c31454ceb60d1a226a938207e4464b
ggshawn <geshuai@nwafu.edu.cn>
2024-10-08 09:43:47 +0800

    a little modification

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -61,8 +61,6 @@ public class NBody{
         }
         StdOut.printf("%d\n", planets.length);
         StdOut.printf("%.2e\n", radius);
-        StdOut.printf("%d\n", planets.length);
-        StdOut.printf("%.2e\n", radius);
         for (int i = 0; i < planets.length; i++) {
             StdOut.printf("%11.4e %11.4e %11.4e %11.4e %11.4e %12s\n",
                         planets[i].xxPos, planets[i].yyPos, planets[i].xxVel,
