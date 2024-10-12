a3ac544c9bb1b2699e42245358fef23dc2369550
ggshawn <geshuai@nwafu.edu.cn>
2024-10-12 17:33:45 +0800

    stuck in resize

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -15,50 +15,38 @@ public class ArrayDeque<T> {
      public ArrayDeque() {
          items = (T[]) new Object[8];
          size = 0;
-         nextFirst = items.length / 2;
-         nextLast = items.length / 2 - 1;
+         nextFirst = items.length / 2 - 1;
+         nextLast = items.length / 2 ;
      }
-     // Invariants: the item to be added is always at the position of size.
+     // Invariants:
      // size equals the number of items
 
      // helper function, a little bit tricky
-     // maybe option3 is better for clear double end
-    /**
+
      public void resize(int x){
          T[] a = (T[]) new Object[x];
          System.arraycopy(items, start,a,start + size, size - start + 1);
          System.arraycopy(items, 0, a, 0, size - start - 1);
          items = a;
      }
-     */
 
      public void addFirst(T x){
-         /**
          if (size == items.length){
              resize(2*size);
          }
-          */
-         if (nextFirst < 0){
-             nextFirst = items.length - 1;
-         }
          items[nextFirst] = x;
-         nextFirst -= 1;
+         nextFirst = (nextFirst - 1 + items.length) % items.length;
          size += 1;
 
 
      }
 
      public void addLast(T x){
-         /**
          if (size == items.length){
              resize(2*size);
          }
-         */
-         if (nextLast > items.length - 1){
-             nextLast = 0;
-         }
          items[nextLast] = x;
-         nextLast += 1;
+         nextLast = (nextLast + 1) % items.length;
          size += 1;
 
      }
@@ -85,9 +73,7 @@ public class ArrayDeque<T> {
          if (isEmpty()){
              return null;
          }
-         // how to change for elegance
          nextFirst = (nextFirst + 1) % items.length;
-
          T firstItem = items[nextFirst];
          items[nextFirst] = null;
          size -= 1;
@@ -111,7 +97,6 @@ public class ArrayDeque<T> {
          if (isEmpty()){
              return null;
          }
-         int p = nextFirst;
-         return items[((p + 1 + index) % items.length)];
+         return items[((nextFirst + 1 + index) % items.length)];
      }
 }
