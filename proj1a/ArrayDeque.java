902292223dfd4bf3a449354fea83d5beb45deae5
ggshawn <geshuai@nwafu.edu.cn>
2024-10-13 11:20:58 +0800

    before adjust form

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,18 +1,16 @@
-import java.util.Map;
-
 public class ArrayDeque<T> {
+    private final static int MIN_CAPACITY = 16;
      private int size;
      private T[] items;
      private int nextFirst;
      private int nextLast;
      // gpt gives me an inspiration to give up start
 
-    /** help thin how to add first
+     /** help thin how to add first
      *  0 1 2 3 4 5 6 7 8 9 10
      * [6,8,9,4,3,
      * possible learn from python list
      */
-
      public ArrayDeque() {
          items = (T[]) new Object[8];
          size = 0;
@@ -23,13 +21,13 @@ public class ArrayDeque<T> {
      // size equals the number of items
 
      // helper function, a little bit tricky
-    // finally choose the option2
+     // finally choose the option2
 
-     public void resize(int x){
+     private void resize(int x){
          T[] a = (T[]) new Object[x];
          int p = (nextFirst + 1) % items.length;
          // cannot use copy directly, must guarantee the right order;
-         for (int i = 0; i < size; i += 1){
+         for (int i = 0; i < size; i += 1) {
              a[i] = items[p];
              p = (p + 1) % items.length;
          }
@@ -39,64 +37,70 @@ public class ArrayDeque<T> {
      }
 
      public void addFirst(T x){
-         if (size == items.length){
-             resize(2*size);
+         if (size == items.length) {
+             resize(2 * items.length);
          }
          items[nextFirst] = x;
          nextFirst = (nextFirst - 1 + items.length) % items.length;
          size += 1;
      }
 
-     public void addLast(T x){
-         if (size == items.length){
-             resize(2*size);
+     public void addLast(T x) {
+         if (size == items.length) {
+             resize(2 * items.length);
          }
          items[nextLast] = x;
          nextLast = (nextLast + 1) % items.length;
          size += 1;
      }
 
-     public boolean isEmpty(){
+     public boolean isEmpty() {
         return size == 0;
      }
 
-     public int size(){
+     public int size() {
          return this.size;
      }
 
-     public void printDeque(){
+     public void printDeque() {
          int p = (nextFirst + 1) % items.length;
-         for (int i = 0; i < size; i += 1){
+         for (int i = 0; i < size; i += 1) {
              System.out.print(items[p] + " ");
              p = (p + 1) % items.length;
          }
      }
 
-     public T removeFirst(){
-         if (isEmpty()){
+     public T removeFirst() {
+         if (isEmpty()) {
              return null;
          }
          nextFirst = (nextFirst + 1) % items.length;
          T firstItem = items[nextFirst];
          items[nextFirst] = null;
          size -= 1;
+         if (((double) size / items.length) < 0.25 && items.length > MIN_CAPACITY) {
+             resize(Math.max((items.length) / 2, MIN_CAPACITY));
+         }
          return firstItem;
     }
 
-     public T removeLast(){
-         if (isEmpty()){
+     public T removeLast() {
+         if (isEmpty()) {
              return null;
          }
          nextLast = (nextLast - 1 + items.length) % items.length ;
          T lastItem = items[nextLast];
          items[nextLast] = null;
          size -= 1;
+         if (((double) size / items.length) < 0.25 && items.length > MIN_CAPACITY) {
+             resize(Math.max((items.length) / 2, MIN_CAPACITY));
+         }
          return lastItem;
      }
 
      // from nextfirst to next last
-     public T get(int index){
-         if (index >= size || index < 0){
+     public T get(int index) {
+         if (index >= size || index < 0) {
              return null;
          }
          return items[((nextFirst + 1 + index) % items.length)];
--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -1,16 +1,14 @@
-import sun.font.TrueTypeFont;
-
 public class LinkedListDeque <Horse> {
-    private StuffNode sentinel;
+    private final StuffNode sentinel;
     private int size;
     private StuffNode last;
 
     private class StuffNode {
-        public StuffNode prev;
-        public Horse item;
-        public StuffNode next;
+        private StuffNode prev;
+        private Horse item;
+        private StuffNode next;
 
-        public StuffNode(StuffNode s1, Horse h, StuffNode s2 ) {
+        private StuffNode(StuffNode s1, Horse h, StuffNode s2 ) {
             prev = s1;
             item = h;
             next = s2;
@@ -19,7 +17,7 @@ public class LinkedListDeque <Horse> {
 
     /** last is a pointer
      * set sentinel item to be null*/
-    public LinkedListDeque (Horse x){
+    public LinkedListDeque(Horse x) {
         size = 1;
         sentinel = new StuffNode(null, null, null);
         sentinel.next = new StuffNode(sentinel, x , sentinel);
@@ -27,16 +25,16 @@ public class LinkedListDeque <Horse> {
         last = sentinel.prev;
     }
 
-    public LinkedListDeque(){
+    public LinkedListDeque() {
         size = 0;
         sentinel = new StuffNode(null, null, null);
-        sentinel.next =sentinel;
+        sentinel.next = sentinel;
         sentinel.prev = sentinel;
         last = sentinel;
     }
 
-    public void addFirst(Horse item){
-        StuffNode newNode= new StuffNode(sentinel, item, sentinel.next);
+    public void addFirst(Horse item) {
+        StuffNode newNode = new StuffNode(sentinel, item, sentinel.next);
         sentinel.next.prev = newNode;
         sentinel.next = newNode;
         size += 1;
@@ -53,12 +51,12 @@ public class LinkedListDeque <Horse> {
         return size == 0;
     }
 
-    public int size(){
+    public int size() {
         return this.size;
     }
 
     /** wait for optimization */
-    public void printDeque(){
+    public void printDeque() {
         StuffNode p = sentinel.next;
         while (p != sentinel){
             System.out.print(p.item + " ");
@@ -66,37 +64,37 @@ public class LinkedListDeque <Horse> {
         }
     }
 
-    public Horse removeFirst(){
-        if (isEmpty()){
+    public Horse removeFirst() {
+        if (isEmpty()) {
             return null;
         }
-        Horse before_first = sentinel.next.item;
+        Horse beforeFirst = sentinel.next.item;
         sentinel.next.next.prev = sentinel;
         sentinel.next = sentinel.next.next;
         size -= 1;
-        if (isEmpty()){
+        if (isEmpty()) {
             last = sentinel;
         }
-        return before_first;
+        return beforeFirst;
     }
 
     /**  is it True? */
-    public Horse removeLast(){
-        if (isEmpty()){
+    public Horse removeLast() {
+        if (isEmpty()) {
             return null;
         }
-        Horse before_last = sentinel.prev.item;
+        Horse beforeLast = sentinel.prev.item;
         sentinel.prev.prev.next = sentinel;
         sentinel.prev = sentinel.prev.prev;
         size -= 1;
-        if (isEmpty()){
+        if (isEmpty()) {
             last = sentinel;
         }
-        return before_last;
+        return beforeLast;
     }
 
     /** how to solve these problems more elegantly*/
-    public Horse get(int index){
+    public Horse get(int index) {
         if (index < 0 || index >= size){
             return null;
         }
@@ -110,15 +108,15 @@ public class LinkedListDeque <Horse> {
 
     // helper function by middleman
     public Horse getRecursive(StuffNode middleman, int index){
-        if (index == 0){
+        if (index == 0) {
             return middleman.next.item;
         }
         return getRecursive(middleman.next, index - 1);
     }
 
 
-    public Horse getRecursive(int index){
-        if (isEmpty()){
+    public Horse getRecursive(int index) {
+        if (isEmpty()) {
             return null;
         }
         return getRecursive(sentinel, index);
