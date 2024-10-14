4e6639f8198d2d1c2912b447fc1f47fcffc73f5d
ggshawn <geshuai@nwafu.edu.cn>
2024-10-14 12:11:05 +0800

    half project 1b

--- proj1b/Palindrome.java
+++ proj1b/Palindrome.java
@@ -1,2 +1,31 @@
 public class Palindrome {
+	public Deque<Character> wordToDeque(String word){
+		Deque<Character> wordDeque = new LinkedListDeque<>();
+		int length = word.length();
+		for (int i = 0; i < length; i++) {
+			wordDeque.addLast(word.charAt(i));
+		}
+		return wordDeque;
+	}
+
+	public boolean isPalindrome(String word){
+		Deque<Character> wordDeque = wordToDeque(word);
+		return tellPalindrome(wordDeque);
+	}
+
+	// helper function
+
+	public boolean tellPalindrome(Deque<Character> characterDeque) {
+		if (characterDeque.isEmpty() || characterDeque.size() == 1) {
+			return true;
+		} else {
+			char a = characterDeque.removeFirst();
+			char b = characterDeque.removeLast();
+			if (a != b) {
+				return false;
+			}else {
+				return tellPalindrome(characterDeque);
+			}
+		}
+	}
 }
