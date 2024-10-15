6ffc0b888d8181106b59fff55d7bdb2b1cd343a2
ggshawn <geshuai@nwafu.edu.cn>
2024-10-15 14:52:58 +0800

    first try proj1 gold

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -7,16 +7,19 @@ public class TestArrayDequeGold {
 	public void testFirst() {
 		StudentArrayDeque<Integer> sad1 = new StudentArrayDeque<>();
 		ArrayDequeSolution<Integer> correct = new ArrayDequeSolution<>();
+		String returnStatement = "";
 
 		for (int i = 0; i < 10; i += 1) {
 			Integer number = StdRandom.uniform(0, 10);
 			sad1.addFirst(number);
+			returnStatement = returnStatement + "addFirst(" + number + ")\n";
 			correct.addFirst(number);
 		}
 		for (int i = 0; i < 10; i += 1) {
 			Integer a = sad1.removeFirst();
+			returnStatement = returnStatement + "removeFirst()\n";
 			Integer b = correct.removeFirst();
-			assertEquals(a,b);
+			assertEquals(returnStatement,a,b);
 		}
 	}
 
@@ -24,16 +27,19 @@ public class TestArrayDequeGold {
 	public void testLast() {
 		StudentArrayDeque<Integer> sad2 = new StudentArrayDeque<>();
 		ArrayDequeSolution<Integer> correct2 = new ArrayDequeSolution<>();
+		String returnStatement = "";
 
 		for (int i = 0; i < 10; i += 1) {
 			Integer number = StdRandom.uniform(-10, 0);
 			sad2.addLast(number);
+			returnStatement = returnStatement + "addLast(" + number + ")\n";
 			correct2.addLast(number);
 		}
 		for (int i = 0; i < 10; i += 1) {
 			Integer a = sad2.removeLast();
+			returnStatement = returnStatement + "removeLast()\n";
 			Integer b = correct2.removeLast();
-			assertEquals(a,b);
+			assertEquals(returnStatement,a,b);
 		}
 	}
 
@@ -42,12 +48,18 @@ public class TestArrayDequeGold {
 		StudentArrayDeque<Integer> sad3 = new StudentArrayDeque<>();
 		ArrayDequeSolution<Integer> correct3 = new ArrayDequeSolution<>();
 		Integer length = StdRandom.uniform(10);
+		String returnStatement = "";
+
 		for (int i = 0; i < length; i += 1) {
 			Integer number = StdRandom.uniform(-10, 0);
 			sad3.addFirst(number);
+			returnStatement = returnStatement + "addFirst(" + number + ")\n";
 			correct3.addFirst(number);
 		}
-		assertEquals(sad3.size(),correct3.size());
+		int a = sad3.size();
+		returnStatement = returnStatement + "size()\n";
+		int b = correct3.size();
+		assertEquals(returnStatement,a,b);
 
 	}
 
@@ -56,23 +68,21 @@ public class TestArrayDequeGold {
 		StudentArrayDeque<Integer> sad5 = new StudentArrayDeque<>();
 		ArrayDequeSolution<Integer> correct5 = new ArrayDequeSolution<>();
 		Integer length = StdRandom.uniform(20);
+		String returnStatement = "";
+
 		for (int i = 0; i < length; i += 1) {
 			Integer number = StdRandom.uniform(-10, 0);
 			sad5.addFirst(number);
+			returnStatement = returnStatement + "addFirst(" + number + ")\n";
 			correct5.addFirst(number);
 		}
 		for (int i = 0; i < length; i += 1) {
 			Integer index =  StdRandom.uniform(0, length);
 			Integer a = sad5.get(index);
-			Integer b = sad5.get(index);
-			assertEquals(a, b);
+			returnStatement = returnStatement + "get(" + index + ")\n";
+			Integer b = correct5.get(index);
+			assertEquals(returnStatement, a, b);
 		}
-	}
-
-	@Test
-	public void testPrintDeque() {
-		
-
 	}
 
 	@Test
@@ -80,16 +90,23 @@ public class TestArrayDequeGold {
 		StudentArrayDeque<Integer> sad4 = new StudentArrayDeque<>();
 		ArrayDequeSolution<Integer> correct4 = new ArrayDequeSolution<>();
 		Integer length = StdRandom.uniform(10);
+		String returnStatement = "";
+
 		for (int i = 0; i < length; i += 1) {
 			Integer number = StdRandom.uniform(-10, 0);
 			sad4.addFirst(number);
+			returnStatement = returnStatement + "addFirst(" + number + ")\n";
 			correct4.addFirst(number);
 		}
 		assertEquals(sad4.isEmpty(), correct4.isEmpty());
 		for (int i = 0; i < length; i += 1) {
 			Integer a = sad4.removeFirst();
-			Integer b = sad4.removeFirst();
+			returnStatement = returnStatement + "removeFirst()\n";
+			Integer b = correct4.removeFirst();
 		}
-		assertEquals(sad4.isEmpty(), correct4.isEmpty());
+		boolean a = sad4.isEmpty();
+		returnStatement = returnStatement + "isEmpty()\n";
+		boolean b = correct4.isEmpty();
+		assertEquals(returnStatement, a, b);
 	}
 }
