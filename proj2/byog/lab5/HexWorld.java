633ae567360419cad7810af1a822177c07f5407a
ggshawn <geshuai@nwafu.edu.cn>
2024-10-16 15:03:10 +0800

    lab5 finished

--- proj2/byog/lab5/HexWorld.java
+++ proj2/byog/lab5/HexWorld.java
@@ -13,11 +13,20 @@ import java.util.Random;
  */
 public class HexWorld {
 	public void addHexgonal(TETile[][] world, position p, int s, TETile t) {
-
+		int[][] fill = ifFill(s);
+		int x = p.startX;
+		int y = p.startY;
+		for (int i = x; i < 3 * s - 2 + x; i += 1) {
+			for (int j = y; j < 2 * s + y; j += 1) {
+				if (fill[i - x][j - y] == 1) {
+					world[i][j] = t;
+				}
+			}
+		}
 	}
 
 	// helper class: start position at the left corner of the hexworld
-	private class position {
+	private static class position {
 		int startX;
 		int startY;
 		public position(int x, int y) {
@@ -36,7 +45,7 @@ public class HexWorld {
 			int count = fillNum(i, s);
 			for (int j = 0; j < count; j = j + 1) {
 				block[startNum][i] = 1;
-				startNum = startNum + 1;
+				startNum += 1;
 			}
 		}
 		return block;
@@ -44,7 +53,7 @@ public class HexWorld {
 
 	// another helper methods for calculating the num of 1 of every row
 	// x starts from 0
-	public static int fillNum(int x, int s) {
+	private static int fillNum(int x, int s) {
 		int num;
 		if (x < s) {
 			num = s + 2 * x;
@@ -56,7 +65,7 @@ public class HexWorld {
 
 	// another helper methods about where to start 1
 	// y starts from 0
-	public static int start1 (int y, int s) {
+	private static int start1 (int y, int s) {
 		int p;
 		if (y < s){
 		    p = s - 1 - y;
@@ -64,5 +73,30 @@ public class HexWorld {
 			p = y - s;
 		}
 		return p;
+	}
+
+
+	// @ source from BoringWorldDemo
+	public static void main(String[] args) {
+		// initialize the tile rendering engine
+		TERenderer ter = new TERenderer();
+		ter.initialize(30, 30);
+
+
+		TETile[][] world = new TETile[30][30];
+		for (int x = 0; x < 30; x += 1) {
+			for (int y = 0; y < 30; y += 1) {
+				world[x][y] = Tileset.NOTHING;
+			}
+		}
+
+
+		// add Hexgonal
+		position p = new position(10, 10);
+		HexWorld hexWorld = new HexWorld();
+		hexWorld.addHexgonal(world,p,4, Tileset.GRASS);
+
+		// draws the world to the screen
+		ter.renderFrame(world);
 	}
 }
